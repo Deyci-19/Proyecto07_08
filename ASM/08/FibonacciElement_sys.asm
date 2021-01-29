36a41bdf97bd5706707703f6760a10b2558661b8
Deyci-19 <66750314+Deyci-19@users.noreply.github.com>
2021-01-29 12:00:23 -0500

    Add files via upload

---
+++ ASM/08/FibonacciElement_sys.asm
@@ -0,0 +1,23 @@
+
+///Iniciando...FibonacciElement_sys.asm
+
+///Traduciendo:FibonacciElement_sys.vm
+
+///function no se encuentra
+
+@4
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+///call no se encuentra
+
+
+///label no se encuentra
+
+
+///goto no se encuentra
+
---
+++ ASM/08/StaticsTest_sys.asm
@@ -0,0 +1,77 @@
+
+///Iniciando...StaticsTest_sys.asm
+
+///Traduciendo:StaticsTest_sys.vm
+
+///function no se encuentra
+
+@6
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+///call no se encuentra
+
+@0
+D=A
+@5
+D=A+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+@23
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@15
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+///call no se encuentra
+
+@0
+D=A
+@5
+D=A+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+///call no se encuentra
+
+
+///call no se encuentra
+
+
+///label no se encuentra
+
+
+///goto no se encuentra
+
